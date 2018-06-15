4cfd11fe5614a73a8ea619eed124ddae9c6b7d38
Alex Sghia-Hughes <alex@mbtace.com>
2018-06-15 12:02:55 -0400

    make the selector more specific

--- assets/js/toggle-all.js
+++ assets/js/toggle-all.js
@@ -1,5 +1,5 @@
 document.querySelector("#toggle-on").addEventListener("click", function(event) {
-  var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  var checkboxes = document.querySelectorAll('.js-sign-toggle');
   for (var i = 0; i < checkboxes.length; i++) {
     checkboxes[i].checked = "checked";
   }
@@ -7,7 +7,7 @@ document.querySelector("#toggle-on").addEventListener("click", function(event) {
 }, false);
 
 document.querySelector("#toggle-off").addEventListener("click", function(event) {
-  var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  var checkboxes = document.querySelectorAll('.js-sign-toggle');
   for (var i = 0; i < checkboxes.length; i++) {
     checkboxes[i].checked = null;
   }
