13e5fceaf9966ffbd42e68b4702f8800a301a131
Alex Sghia-Hughes <alex@mbtace.com>
2018-07-26 10:41:23 -0400

    fix test

--- test/signs_ui/signs/messages_test.exs
+++ test/signs_ui/signs/messages_test.exs
@@ -3,7 +3,14 @@ defmodule SignsUi.Signs.MessagesTest do
 
   test "receives a message and stores the sign in the state" do
     {:ok, pid} = SignsUi.Signs.Messages.start_link()
-    SignsUi.Signs.Messages.add_message(pid, %{"MsgType" => "SignContent", "c" => "e130~n2-\"Alewife 12 min\"", "sta" => "RDTC", "uid" => "616722"})
-    assert SignsUi.Signs.Messages.list_messages(pid) == %{"RDTC-n" => ["", "\"Alewife 12 min\""]}
+
+    SignsUi.Signs.Messages.add_message(pid, %{
+      "MsgType" => "SignContent",
+      "c" => "e130~n2-\"Alewife 12 min\"",
+      "sta" => "RDTC",
+      "uid" => "616722"
+    })
+
+    assert SignsUi.Signs.Messages.list_messages(pid) == %{"RDTC-n" => ["", "Alewife 12 min"]}
   end
 end
