f91a4a9d0b8d52124a788583bb3f3dec6ab717af
Alex Sghia-Hughes <alex@mbtace.com>
2018-07-27 11:32:11 -0400

    dont do the raw body thing

--- lib/signs_ui_web/realtime_signs_body_reader.ex
+++ lib/signs_ui_web/realtime_signs_body_reader.ex
@@ -1,7 +1,6 @@
 defmodule RealtimeSignsBodyReader do
   def read_body(conn, opts) do
     {:ok, body, conn} = Plug.Conn.read_body(conn, opts)
-    conn = update_in(conn.assigns[:raw_body], &[body | &1 || []])
 
     body_params =
       body
