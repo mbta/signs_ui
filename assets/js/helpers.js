69e7ec413b14da18dd95720737379be6a39a5e85
Alex Sghia-Hughes <alex@mbtace.com>
2018-08-02 14:38:21 -0400

    test issues

--- assets/js/helpers.js
+++ assets/js/helpers.js
@@ -1,7 +1,7 @@
 function updateSigns(oldSigns, {
   signId, lineNumber, content, duration,
 }) {
-  const newValues = oldSigns[signId] ? oldSigns[signId].slice(0) : [{ text: '', duration: '0' }, { text: '', duration: '0' }];
+  const newValues = oldSigns[signId] ? oldSigns[signId].slice(0) : [{ content: '', duration: '0' }, { content: '', duration: '0' }];
   newValues[lineNumber - 1] = { content, duration };
 
   return {
