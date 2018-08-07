50239e742281e6da0e7303e3cbeb7bb81f1e8be0
Alex Sghia-Hughes <asghiah@gmail.com>
2018-08-07 16:33:24 -0400

    quick fix for the crash if we get a multipart message (#39)
    * quick fix for the crash if we get a multipart message
    
    * only show the headsign and time in the messages
    
    * actual period, no wildcard

--- test/signs_ui/signs/messages_test.exs
+++ test/signs_ui/signs/messages_test.exs
@@ -14,4 +14,18 @@ defmodule SignsUi.Signs.MessagesTest do
     assert %{"RDTC-n" => [%{duration: _, text: ""}, %{duration: _, text: "Alewife 12 min"}]} =
              SignsUi.Signs.Messages.list_messages(pid)
   end
+
+  test "when the message has multiple dashes it doesnt crash" do
+    {:ok, pid} = SignsUi.Signs.Messages.start_link()
+
+    SignsUi.Signs.Messages.add_message(pid, %{
+      "MsgType" => "SignContent",
+      "c" => ["e130~n2-\"Alewife 12 min\".4-\"Braintre 4 min\""],
+      "sta" => "RDTC",
+      "uid" => "616722"
+    })
+
+    assert %{"RDTC-n" => [%{duration: _, text: ""}, %{duration: _, text: "Alewife 12 min"}]} =
+             SignsUi.Signs.Messages.list_messages(pid)
+  end
 end
