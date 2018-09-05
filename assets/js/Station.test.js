53af860f3994d52d32b38513267c3b95ec8842a8
Gabe Durazo <gabe@mbtace.com>
2018-09-05 08:08:41 -0500

    Tweak CSS, add Station.jsx test

---
+++ assets/js/Station.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+import Station from './Station';
+
+test('shows the custom configuration information for a station', () => {
+  const config = {
+    id: 'OMAL',
+    name: 'Malden Center',
+    zones: {
+      n: false, s: false, e: false, w: false, c: false, m: 'Entire Station',
+    },
+  };
+  const currentTime = Date.now() + 2000;
+  const signs = {
+    'OMAL-m': [
+      { text: 'Alewife 1 min', duration: (currentTime + 1000).toLocaleString() },
+      { text: 'Alewife 3 min', duration: (currentTime + 1000).toLocaleString() },
+    ],
+  };
+  const line = 'Orange';
+
+  const wrapper = mount(
+    React.createElement(Station, {
+      config, signs, currentTime, line,
+    }),
+  );
+
+  expect(wrapper.text()).toMatch('Entire Station');
+});
