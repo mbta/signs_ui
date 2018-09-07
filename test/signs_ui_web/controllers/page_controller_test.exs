d49fd3693aaa22d202f3942e3098002cea900331
Gabe Durazo <gabe@mbtace.com>
2018-09-07 10:31:16 -0500

    Update test for redirect of home page

--- test/signs_ui_web/controllers/page_controller_test.exs
+++ test/signs_ui_web/controllers/page_controller_test.exs
@@ -4,6 +4,6 @@ defmodule SignsUiWeb.PageControllerTest do
   test "GET /", %{conn: conn} do
     conn = %{conn | req_headers: [{"authorization", "Basic dXNlcm5hbWU6cGFzc3dvcmQ="}]}
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "All signs"
+    assert html_response(conn, 302) =~ "/viewer"
   end
 end
