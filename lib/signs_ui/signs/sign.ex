69ee64083604645d743ecc4b9140e558948bdf5d
Gabe Durazo <gabe@mbtace.com>
2019-01-16 16:10:18 -0500

    set enabled: true for static text for now

--- lib/signs_ui/signs/sign.ex
+++ lib/signs_ui/signs/sign.ex
@@ -177,7 +177,7 @@ defmodule SignsUI.Signs.Sign do
     %{
       "id" => sign.id,
       "mode" => "static_text",
-      "enabled" => false,
+      "enabled" => true,
       "line1" => sign.config.line1,
       "line2" => sign.config.line2,
       "expires" => sign.config.expires
