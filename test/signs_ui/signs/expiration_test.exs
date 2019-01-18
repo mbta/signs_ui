ebdf2dfb0e4caa2fdff2e8fa1e06ed079377b9a8
Eddie Maldonado <emaldonado@mbta.com>
2019-01-18 14:00:38 -0500

    More test cases

--- test/signs_ui/signs/expiration_test.exs
+++ test/signs_ui/signs/expiration_test.exs
@@ -1,10 +1,54 @@
 defmodule SignsUI.Signs.ExpirationTest do
   use ExUnit.Case
-  import SignsUI.Signs.Expiration
+  alias SignsUI.Signs.Sign
+
+  describe "expire_signs/2" do
+    test "expire_signs/2" do
+      {:ok, time1, 0} = DateTime.from_iso8601("2019-01-15T12:00:00Z")
+      {:ok, time2, 0} = DateTime.from_iso8601("2019-01-15T14:00:00Z")
+
+      signs = %{
+        "nil_expiration" => %Sign{
+          id: "nil_expiration",
+          config: %{
+            mode: :off,
+            expires: nil
+          }
+        },
+        "past_expiration" => %Sign{
+          id: "past_expiration",
+          config: %{
+            mode: :off,
+            expires: time1
+          }
+        },
+        "future_expiration" => %Sign{
+          id: "future_expiration",
+          config: %{
+            mode: :off,
+            expires: time2
+          }
+        }
+      }
+
+      expected_updates = %{
+        "past_expiration" => %Sign{
+          id: "past_expiration",
+          config: %{
+            mode: :auto
+          }
+        }
+      }
+
+      assert SignsUI.Signs.Expiration.expire_signs(signs, fn ->
+               Timex.to_datetime(~N[2019-01-15 08:00:00], "America/New_York")
+             end) == expected_updates
+    end
+  end
 
   describe "process_expired callback" do
     test "expires expired sign, but not signs that haven't expired" do
-      {:ok, state_pid} = SignsUI.Signs.State.start_link(name: :sign_state_test)
+      {:ok, _state_pid} = SignsUI.Signs.State.start_link(name: :sign_state_test)
 
       state = %{
         time_fetcher: fn ->
