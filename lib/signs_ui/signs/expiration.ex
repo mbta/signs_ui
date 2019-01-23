b50f6b0fabc5f3a04a39d81945569568e0283f54
Eddie Maldonado <emaldonado@mbta.com>
2019-01-23 13:07:17 -0500

    Add call to broadcast! on expiration

--- lib/signs_ui/signs/expiration.ex
+++ lib/signs_ui/signs/expiration.ex
@@ -37,7 +37,14 @@ defmodule SignsUI.Signs.Expiration do
 
     if updates != %{} do
       Logger.info("Cleaning expired settings for sign IDs: #{inspect(Map.keys(updates))}")
-      SignsUI.Signs.State.update_some(state.sign_state_server, updates)
+      {:ok, new_state} = SignsUI.Signs.State.update_some(state.sign_state_server, updates)
+
+      broadcast_data =
+        new_state
+        |> Enum.map(fn {_id, sign} -> {sign.id, sign.config} end)
+        |> Enum.into(%{})
+
+      SignsUiWeb.Endpoint.broadcast!("signs:all", "new_sign_config_state", broadcast_data)
     end
 
     schedule_loop(self(), state.loop_ms)
