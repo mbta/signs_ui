3906ebf30bc8ce9564b5411764092bec6857e811
Gabe Durazo <gabe@mbtace.com>
2019-01-29 09:14:15 -0600

    Hide DOCKER_REPO var when running codecov

--- semaphore/static_analysis.sh
+++ semaphore/static_analysis.sh
@@ -3,7 +3,7 @@ set -e
 set -x
 
 MIX_ENV=test mix coveralls.json
-bash <(curl -s https://codecov.io/bash) -t $CODECOV_TOKEN
+DOCKER_REPO="" bash <(curl -s https://codecov.io/bash) -t $CODECOV_TOKEN
 
 # copy any pre-built PLTs to the right directory
 find $SEMAPHORE_CACHE_DIR -name "dialyxir_*_deps-test.plt*" | xargs -I{} cp '{}' _build/test
