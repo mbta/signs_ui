c0086cd1d369b991d6e218f4546cca791866c058
Maldonado <emaldonado@mbta.com>
2019-05-14 16:36:05 -0400

    Redirect to auth workflow when not authenticated

--- lib/signs_ui_web/auth_manager/error_handler.ex
+++ lib/signs_ui_web/auth_manager/error_handler.ex
@@ -1,14 +1,11 @@
 defmodule SignsUiWeb.AuthManager.ErrorHandler do
-  import Plug.Conn
-
   @behaviour Guardian.Plug.ErrorHandler
 
   @impl Guardian.Plug.ErrorHandler
-  def auth_error(conn, {type, _reason}, _opts) do
-    body = to_string(type)
-
+  def auth_error(conn, {_type, _reason}, _opts) do
     conn
-    |> put_resp_content_type("text/plain")
-    |> send_resp(401, body)
+    |> Phoenix.Controller.redirect(
+      to: SignsUiWeb.Router.Helpers.auth_path(conn, :request, "cognito")
+    )
   end
 end
--- test/signs_ui_web/auth_manager/error_handler_test.exs
+++ test/signs_ui_web/auth_manager/error_handler_test.exs
@@ -2,10 +2,10 @@ defmodule SignsUiWeb.AuthManager.ErrorHandlerTest do
   use SignsUiWeb.ConnCase
 
   describe "auth_error/3" do
-    test "sends 401 response", %{conn: conn} do
+    test "redirects to Cognito login", %{conn: conn} do
       conn = SignsUiWeb.AuthManager.ErrorHandler.auth_error(conn, {:some_type, :reason}, [])
 
-      assert text_response(conn, 401) =~ "some_type"
+      assert html_response(conn, 302) =~ "/auth/cognito"
     end
   end
 end
