cd9674737ec6669f0d08e3864096cc919ce8fe14
Maldonado <emaldonado@mbta.com>
2019-07-12 12:08:08 -0400

    Clear the refresh token when using it

--- lib/signs_ui_web/auth_manager/error_handler.ex
+++ lib/signs_ui_web/auth_manager/error_handler.ex
@@ -5,13 +5,16 @@ defmodule SignsUiWeb.AuthManager.ErrorHandler do
   def auth_error(conn, {_type, _reason}, _opts) do
     refresh_token_store = Application.get_env(:signs_ui, :refresh_token_store)
 
-    refresh_token =
+    username =
       conn
       |> Plug.Conn.fetch_session()
       |> Plug.Conn.get_session(:signs_ui_username)
-      |> refresh_token_store.get_refresh_token()
+
+    refresh_token = refresh_token_store.get_refresh_token(username)
 
     if refresh_token do
+      refresh_token_store.clear_refresh_token(username)
+
       Phoenix.Controller.redirect(
         conn,
         to:
