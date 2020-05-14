56cd3900ec0b42cbbdd6922cf64c49b1c9a2f444
Frank LaNasa <fjlanasa@Franks-MacBook-Pro.local>
2020-05-14 09:31:57 -0400

    chore: bump frontend deps

--- assets/scripts/jestSetup.js
+++ assets/scripts/jestSetup.js
@@ -1,3 +1,4 @@
+require("regenerator-runtime/runtime");
 const Enzyme = require('enzyme');
 const EnzymeAdapter = require('enzyme-adapter-react-16');
 Enzyme.configure({ adapter: new EnzymeAdapter() });
