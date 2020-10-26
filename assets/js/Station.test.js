4bfb4b6233e18516443ad2099d5b5711836d580c
Maldonado <emaldonado@mbta.com>
2020-10-26 14:38:27 -0400

    feat: allow custom positioning of signs

--- assets/js/Station.test.js
+++ assets/js/Station.test.js
@@ -37,3 +37,43 @@ test('shows the custom configuration information for a station', () => {
 
   expect(wrapper.text()).toMatch('Entire Station');
 });
+
+test('allows custom reordering of sign positions', () => {
+  const config = {
+    id: 'OGRE',
+    name: 'Green St',
+    zonePositions: {
+      left: ['n'],
+      center: [],
+      right: ['s'],
+    },
+    zones: {
+      n: { value: 'foo' },
+      s: { value: 'bar' },
+      e: { value: false },
+      w: { value: false },
+      c: { value: false },
+      m: { value: false },
+    },
+  };
+  const currentTime = Date.now() + 2000;
+  const signs = {};
+  const line = 'Orange';
+  const signConfigs = {};
+  const setConfigs = () => {};
+  const readOnly = false;
+
+  const wrapper = mount(
+    React.createElement(Station, {
+      config,
+      signs,
+      currentTime,
+      line,
+      signConfigs,
+      setConfigs,
+      readOnly,
+    }),
+  );
+
+  expect(wrapper.text()).toMatch(/foo.*bar/);
+});
