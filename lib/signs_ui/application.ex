bc71de7589a4b3a8a1f39bf3347f0700890ccc31
Cora Grant <394835+x@users.noreply.github.com>
2020-10-30 16:40:57 -0400

    feat: enable multiple API keys for message posts
    This expects a `MESSAGES_API_KEYS` variable in the environment with a
    format like `user1:key1,user2:key2,...`. The "user" is a label only used
    for logging; there is no change to how clients authenticate.

--- lib/signs_ui/application.ex
+++ lib/signs_ui/application.ex
@@ -28,6 +28,6 @@ defmodule SignsUi.Application do
   defp set_runtime_config do
     Config.update_env(:aws_signs_bucket, System.get_env("AWS_SIGNS_BUCKET"))
     Config.update_env(:aws_signs_path, System.get_env("AWS_SIGNS_PATH"))
-    Config.update_env(:realtime_signs_api_key, System.get_env("REALTIME_SIGNS_API_KEY"))
+    Config.update_env(:messages_api_keys, System.get_env("MESSAGES_API_KEYS"))
   end
 end
