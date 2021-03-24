5807ac854d9b4c233ace5b5bb3cf2afa79ad3a87
Beatrix Klebe <bklebe@mbta.com>
2021-03-24 15:04:11 -0400

    refactor: merge Config.update_env definitions

--- lib/utilities/config.ex
+++ lib/utilities/config.ex
@@ -1,11 +1,6 @@
 defmodule SignsUi.Utilities.Config do
-  def update_env(key, val) do
-    if is_nil(Application.get_env(:signs_ui, key)) do
-      Application.put_env(:signs_ui, key, val)
-    end
-  end
-
-  def update_env(app, key, val) do
+  @spec update_env(atom, atom, String.t() | nil) :: nil | :ok
+  def update_env(app \\ :signs_ui, key, val) do
     if is_nil(Application.get_env(app, key)) do
       Application.put_env(app, key, val)
     end
