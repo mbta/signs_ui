35a13aaa3a3e1ad60ffbf1ca1c6a42447bccb8c7
Matthew Kennedy <mkennedy2@mbta.com>
2021-04-26 14:12:57 -0400

    using SignsUi.Alerts.State.state in typespec

--- lib/signs_ui/config/expiration.ex
+++ lib/signs_ui/config/expiration.ex
@@ -102,7 +102,7 @@ defmodule SignsUi.Config.Expiration do
     end
   end
 
-  @spec get_active_alert_ids(atom | %{:alerts => map, optional(any) => any}) ::
+  @spec get_active_alert_ids(SignsUi.Alerts.State.state()) ::
           MapSet.t(String.t())
   def get_active_alert_ids(alert_state) do
     alerts = alert_state.alerts
