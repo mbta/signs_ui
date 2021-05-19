ae3e90176c943624fcd890b39e8d4135eef73d22
Beatrix Klebe <bklebe@mbta.com>
2021-05-19 14:33:06 -0400

    refactor: record Sentry events if DSN is present

--- lib/signs_ui_web/views/layout_view.ex
+++ lib/signs_ui_web/views/layout_view.ex
@@ -1,8 +1,3 @@
 defmodule SignsUiWeb.LayoutView do
   use SignsUiWeb, :view
-  @spec record_sentry?() :: boolean()
-  def record_sentry? do
-    sentry_env = Application.get_env(:sentry, :environment_name)
-    sentry_env == "dev" || sentry_env == "prod"
-  end
 end
