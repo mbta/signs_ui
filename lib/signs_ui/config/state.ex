63c4bf98fa366b8ccf04bdf9d55e96fb2f96445b
Matthew Kennedy <mkennedy2@mbta.com>
2021-05-27 20:28:15 -0400

    @type t matches branch bak-sign-groups-expire

--- lib/signs_ui/config/state.ex
+++ lib/signs_ui/config/state.ex
@@ -6,14 +6,12 @@ defmodule SignsUi.Config.State do
   require Logger
 
   alias SignsUi.Config
+  alias SignsUi.Config.ConfiguredHeadways
+  alias SignsUi.Config.SignGroups
 
   @type t :: %{
-          signs: %{
-            Config.Sign.id() => Config.Sign.t()
-          },
-          configured_headways: %{
-            String.t() => %{String.t() => Config.ConfiguredHeadway.t()}
-          },
+          signs: %{Config.Sign.id() => Config.Sign.t()},
+          configured_headways: ConfiguredHeadways.t(),
           chelsea_bridge_announcements: String.t(),
           sign_groups: SignGroups.t()
         }
