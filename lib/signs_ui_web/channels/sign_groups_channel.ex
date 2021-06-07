e8d2d0199bf3c23bcc9d98bce5914ccb8408ba78
Matthew Kennedy <mkennedy2@mbta.com>
2021-06-07 16:30:53 -0400

    created a new map with expected changes

--- lib/signs_ui_web/channels/sign_groups_channel.ex
+++ lib/signs_ui_web/channels/sign_groups_channel.ex
@@ -15,6 +15,52 @@ defmodule SignsUiWeb.SignGroupsChannel do
   def handle_in("changeSignGroups", %{"route" => route, "data" => data} = changes, socket) do
     Logger.info(["changeSignGroups: ", inspect(changes), ", from: ", inspect(socket)])
 
+    data = changes["data"]
+    [time | _] = Map.keys(data)
+    line1 = data[time]["line1"]
+    line2 = data[time]["line2"]
+    expires = data[time]["expires"]
+    alert_id = data[time]["alert_id"]
+    sign_ids = data[time]["sign_ids"]
+
+    IO.puts("line1 = #{line1}")
+    IO.puts("line2 = #{line2}")
+    IO.puts("expires = #{expires}")
+    IO.puts("alert_id = #{alert_id}")
+    IO.puts("sign_ids\n#{sign_ids}")
+
+    #for sign_id <- sign_ids, do: changes = Map.put(changes, sign_id, %{
+    #  id: sign_id,
+    #  config: %{
+    #    mode: :static_text,
+    #    line1: line1,
+    #    line2: line2,
+    #    expires: expires,
+    #    alert_id: alert_id
+    #  }
+    #})
+
+    changes = Map.new(
+      sign_ids, fn sign_id -> {
+        sign_id, %{
+          id: sign_id,
+          config: %{
+            mode: :static_text,
+            line1: line1,
+            line2: line2,
+            expires: expires,
+            alert_id: alert_id
+          }
+        }
+      }
+    end)
+
+    #changes = Map.put(changes, "test", "VALUE")
+
+
+    IO.puts("CHANGES HAVE BEEN MADE\n#{inspect(changes)}")
+
+    Logger.info(["changeSignGroups: ", inspect(changes), ", from: ", inspect(socket)])
     {:noreply, socket}
   end
 end
