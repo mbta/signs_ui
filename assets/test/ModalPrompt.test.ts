1410c395e81d7aeea5e88b217a781996bb492e47
Gabe Durazo <gdurazo@mbta.com>
2021-06-30 10:59:04 -0500

    feat: ModalPrompt widget

---
+++ assets/test/ModalPrompt.test.ts
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { render, screen, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import ModalPrompt from '../js/ModalPrompt';
+
+test('can accept or cancel the prompt', () => {
+  let isAccepted = false;
+
+  render(
+    React.createElement(ModalPrompt, {
+      acceptText: 'Yes, accept',
+      onAccept: () => {
+        isAccepted = true;
+      },
+      onCancel: () => {
+        isAccepted = false;
+      },
+      contents: React.createElement(
+        'p',
+        {},
+        'This is a prompt. Would you like to accept?',
+      ),
+    }),
+  );
+
+  userEvent.click(screen.getByRole('button', { name: 'Yes, accept' }));
+  expect(isAccepted).toBe(true);
+
+  userEvent.click(screen.getByRole('button', { name: 'Cancel' }));
+  expect(isAccepted).toBe(false);
+});
