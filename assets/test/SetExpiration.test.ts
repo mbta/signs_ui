6e5e232ca6791574f7653ab3c07a59692c805df2
Beatrix Klebe <bklebe@mbta.com>
2021-07-13 10:41:12 -0400

    feat: better selections for "Return to Auto"
    refactor: pass functions directly instead of new closures
    
    refactor: use union instead of boolean flags
    
    fix: remove callback for clearing expiration
    
    feat: restructure right side of sign group editor with grid

--- assets/test/SetExpiration.test.ts
+++ assets/test/SetExpiration.test.ts
@@ -2,86 +2,115 @@ import * as React from 'react';
 import { mount } from 'enzyme';
 
 import SetExpiration from '../js/SetExpiration';
-import { SignConfigs } from '../js/types';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 
 test('Can set the expiration time', () => {
-  const requests: SignConfigs[] = [];
+  const dates: (Date | null)[] = [];
   const readOnly = false;
 
   const wrapper = mount(
     React.createElement(SetExpiration, {
       alerts: {},
-      realtimeId: 'rtID',
-      signConfig: {
-        id: '1',
-        mode: 'static_text',
-        line1: 'line1',
-        line2: 'line2',
-        expires: null,
-      },
-      setConfigs: (arg) => {
-        requests.push(arg);
-      },
+      expires: null,
+      alertId: null,
+      onDateChange: (d) => dates.push(d),
+      onAlertChange: () => true,
       readOnly,
       showAlertSelector: true,
     }),
   );
 
+  wrapper.find('input[type="checkbox"]').simulate('click');
+  wrapper.find('.set_expiration--datetime').simulate('change');
   wrapper.find('.react-datepicker-wrapper input').simulate('focus');
   wrapper.find('.react-datepicker__day--015').simulate('click');
-  expect(requests.length).toBe(1);
-  const newConf = requests[0].rtID;
-  expect(new Date(newConf.expires!)).toBeInstanceOf(Date);
+  expect(dates.length).toBe(1);
+  expect(dates[0]?.getDate()).toBe(15);
 });
 
 test('Can clear the expiration time', () => {
-  const requests: SignConfigs[] = [];
+  const dates: (Date | null)[] = [];
   const readOnly = false;
 
-  const wrapper = mount(
+  render(
     React.createElement(SetExpiration, {
-      alerts: {},
-      realtimeId: 'rtID',
-      signConfig: {
-        id: '1',
-        mode: 'static_text',
-        line1: 'line1',
-        line2: 'line2',
-        expires: new Date().toISOString(),
+      alerts: {
+        Red: {
+          id: 'bar',
+          created_at: null,
+          service_effect: 'nothing',
+        },
       },
-      setConfigs: (arg) => {
-        requests.push(arg);
-      },
+      expires: null,
+      alertId: null,
+      onDateChange: (d) => dates.push(d),
+      onAlertChange: () => true,
       readOnly,
       showAlertSelector: true,
     }),
   );
 
-  wrapper.find('.set_expiration--cancel').simulate('click');
-  expect(requests.length).toBe(1);
-  const newConf = requests[0].rtID;
-  expect(newConf.expires).toBe(null);
+  userEvent.click(
+    screen.getByRole('checkbox', { name: 'Schedule return to "Auto"' }),
+  );
+  userEvent.click(screen.getByRole('radio', { name: 'Date and time' }));
+  userEvent.click(
+    screen.getByRole('radio', { name: 'At the end of an alert effect period' }),
+  );
+
+  expect(dates.length).toBe(1);
+  expect(dates[0]).toBe(null);
+});
+
+test('Deselecting return to auto clears the expiration ', () => {
+  const dates: (Date | null)[] = [];
+  const alerts: (string | null)[] = [];
+  const readOnly = false;
+
+  render(
+    React.createElement(SetExpiration, {
+      alerts: {
+        Red: {
+          id: 'bar',
+          created_at: null,
+          service_effect: 'nothing',
+        },
+      },
+      expires: null,
+      alertId: null,
+      onDateChange: (d) => dates.push(d),
+      onAlertChange: (a) => alerts.push(a),
+      readOnly,
+      showAlertSelector: true,
+    }),
+  );
+
+  userEvent.click(
+    screen.getByRole('checkbox', { name: 'Schedule return to "Auto"' }),
+  );
+  userEvent.click(
+    screen.getByRole('checkbox', { name: 'Schedule return to "Auto"' }),
+  );
+
+  expect(dates.length).toBe(1);
+  expect(alerts.length).toBe(1);
+  expect(dates[0]).toBe(null);
+  expect(alerts[0]).toBe(null);
 });
 
 test('Shows widget when no expiration set if not in read-only mode', () => {
-  const setConfigs = () => true;
-
   const readOnly = false;
 
   const wrapper = mount(
     React.createElement(SetExpiration, {
       alerts: {},
-      realtimeId: 'rtID',
-      signConfig: {
-        id: '1',
-        mode: 'static_text',
-        line1: 'line1',
-        line2: 'line2',
-        expires: null,
-      },
-      setConfigs,
+      expires: null,
+      alertId: null,
+      onDateChange: () => true,
+      onAlertChange: () => true,
       readOnly,
       showAlertSelector: true,
     }),
@@ -91,22 +120,15 @@ test('Shows widget when no expiration set if not in read-only mode', () => {
 });
 
 test('Suppresses widget when no expiration set if in read-only mode', () => {
-  const setConfigs = () => true;
-
   const readOnly = true;
 
   const wrapper = mount(
     React.createElement(SetExpiration, {
       alerts: {},
-      realtimeId: 'rtID',
-      signConfig: {
-        id: '1',
-        mode: 'static_text',
-        line1: 'line1',
-        line2: 'line2',
-        expires: '',
-      },
-      setConfigs,
+      expires: null,
+      alertId: null,
+      onDateChange: () => true,
+      onAlertChange: () => true,
       readOnly,
       showAlertSelector: true,
     }),
@@ -116,22 +138,15 @@ test('Suppresses widget when no expiration set if in read-only mode', () => {
 });
 
 test("Shows 'Scheduled' when expiration is set if in read-only mode", () => {
-  const setConfigs = () => true;
-
   const readOnly = true;
 
   const wrapper = mount(
     React.createElement(SetExpiration, {
       alerts: {},
-      realtimeId: 'rtID',
-      signConfig: {
-        id: '1',
-        mode: 'static_text',
-        line1: 'line1',
-        line2: 'line2',
-        expires: new Date().toISOString(),
-      },
-      setConfigs,
+      expires: new Date(),
+      alertId: null,
+      onDateChange: () => true,
+      onAlertChange: () => true,
       readOnly,
       showAlertSelector: true,
     }),
