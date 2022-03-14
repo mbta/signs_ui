c847d4213716b8db76af5643c1fdb90c97226e5d
Paul Kim <pkim@mbta.com>
2022-03-14 16:29:13 -0400

    Remove usage of refresh token

--- test/signs_ui_web/auth_manager/error_handler_test.exs
+++ test/signs_ui_web/auth_manager/error_handler_test.exs
@@ -1,13 +1,5 @@
 defmodule SignsUiWeb.AuthManager.ErrorHandlerTest do
   use SignsUiWeb.ConnCase
-  import Test.Support.Helpers
-
-  setup do
-    reassign_env(
-      :refresh_token_store,
-      SignsUiWeb.AuthManager.ErrorHandlerTest.FakeRefreshTokenStore
-    )
-  end
 
   describe "auth_error/3" do
     test "redirects to Cognito login if there's no refresh key", %{conn: conn} do
@@ -18,19 +10,5 @@ defmodule SignsUiWeb.AuthManager.ErrorHandlerTest do
 
       assert html_response(conn, 302) =~ "\"/auth/cognito\""
     end
-
-    test "redirects to Cognito callback if there is a refresh key", %{conn: conn} do
-      conn =
-        conn
-        |> init_test_session(%{signs_ui_username: "bar@mbta.com"})
-        |> SignsUiWeb.AuthManager.ErrorHandler.auth_error({:some_type, :reason}, [])
-
-      assert html_response(conn, 302) =~ "\"/auth/cognito/callback\?"
-    end
-  end
-
-  defmodule FakeRefreshTokenStore do
-    def get_refresh_token("foo@mbta.com"), do: nil
-    def get_refresh_token("bar@mbta.com"), do: "abcde"
   end
 end
