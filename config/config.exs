fe0cc173dd93dfc5334a74347561126327f1f8fb
Paul Kim <pkim@mbta.com>
2022-04-19 19:23:19 -0400

    Add releases file and change to import Config

--- config/config.exs
+++ config/config.exs
@@ -1,9 +1,4 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
-#
-# This configuration file is loaded before any dependency and
-# is restricted to this project.
-use Mix.Config
+import Config
 
 # Configures the endpoint
 config :signs_ui, SignsUiWeb.Endpoint,
