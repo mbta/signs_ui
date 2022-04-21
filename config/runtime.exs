4f42d6201684eac8a316e07c5761223dbaf1ce62
Paul Kim <pkim@mbta.com>
2022-04-21 09:23:33 -0400

    only fetch secret_key_base if prod

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,3 +1,5 @@
 import Config
 
-config :signs_ui, SignsUiWeb.Endpoint, secret_key_base: System.fetch_env!("SECRET_KEY_BASE")
+if config_env() == :prod do
+  config :signs_ui, SignsUiWeb.Endpoint, secret_key_base: System.fetch_env!("SECRET_KEY_BASE")
+end
