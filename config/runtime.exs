301ff9f987dfe15b3a72558a06504878a1c77a62
Paul Kim <pkim@mbta.com>
2022-05-04 12:44:14 -0400

    only overwrite secret_key_base in prod

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,4 +1,6 @@
 import Config
 
-config :signs_ui, SignsUiWeb.Endpoint,
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+if config_env() == :prod do
+  config :signs_ui, SignsUiWeb.Endpoint,
+    secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+end
