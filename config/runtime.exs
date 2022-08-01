280b526eee672aed56ee86404f28e68a9779e4c0
Paul Kim <pkim@mbta.com>
2022-08-01 19:05:58 -0400

    remove from runtime.exs

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,8 +1,5 @@
 import Config
 
-config :signs_ui, SignsUiWeb.Endpoint,
-  screenplay_base_url: "https://screenplay-dev.mbtace.com/"
-
 if config_env() == :prod do
   config :signs_ui, SignsUiWeb.Endpoint,
     secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
