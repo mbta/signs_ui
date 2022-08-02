9bd8d0464f9135686d4493ac40939200de2c830d
Paul Kim <pkim@mbta.com>
2022-08-02 10:30:21 -0400

    Allow screenplay to embed PAESS signs using iframes (#805)
    * allow screenplay to embed PAESS signs using iframes
    
    * move configuring of excepted url to router.ex
    
    * remove from runtime.exs
    
    * resolve credo check

--- lib/signs_ui_web/router.ex
+++ lib/signs_ui_web/router.ex
@@ -9,7 +9,11 @@ defmodule SignsUiWeb.Router do
 
     plug(:put_secure_browser_headers, %{
       "content-security-policy" =>
-        "default-src 'self'; connect-src 'self' https://*.ingest.sentry.io; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'"
+        "default-src 'self'; connect-src 'self' https://*.ingest.sentry.io; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; frame-ancestors #{
+          :signs_ui
+          |> Application.get_env(SignsUiWeb.Endpoint)
+          |> Keyword.get(:screenplay_base_url)
+        };"
     })
   end
 
