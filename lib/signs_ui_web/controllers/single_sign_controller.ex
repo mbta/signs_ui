0e5c648d13dcd96730245852c92e02bd35b52544
Paul Kim <pkim@mbta.com>
2022-08-03 14:10:41 -0400

    move addition of header to controller

--- lib/signs_ui_web/controllers/single_sign_controller.ex
+++ lib/signs_ui_web/controllers/single_sign_controller.ex
@@ -8,8 +8,19 @@ defmodule SignsUiWeb.SingleSignController do
     sign_id = station_code <> "-" <> zone
     sign = State.get_single_sign(sign_id)
 
+    [policy] = get_resp_header(conn, "content-security-policy")
+
     conn
     |> put_layout("single_sign.html")
+    |> put_resp_header(
+      "content-security-policy",
+      policy <>
+        " frame-ancestors #{
+          :signs_ui
+          |> Application.get_env(SignsUiWeb.Endpoint)
+          |> Keyword.get(:screenplay_base_url)
+        };"
+    )
     |> render("single_sign.html", sign: sign)
   end
 end
