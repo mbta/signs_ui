2afee35b3966499857e947ba7ccf5c9b229e07e9
Kim <pkim@mbta.com>
2023-03-02 11:46:25 -0500

    remove enzyme from jest setup, package.json, and package-lock.json

--- assets/scripts/jest-setup.ts
+++ assets/scripts/jest-setup.ts
@@ -1,7 +1,3 @@
 require('regenerator-runtime/runtime');
 
-const Enzyme = require('enzyme');
-const EnzymeAdapter = require('enzyme-adapter-react-16');
-Enzyme.configure({ adapter: new EnzymeAdapter() });
-
 import '@testing-library/jest-dom';
