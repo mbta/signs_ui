4ac80574e80da1d27d86176abf03f9f24958d078
cmaddox5 <christian.maddox93@gmail.com>
2024-09-04 09:22:19 -0400

    AuthController migration to Keycloak.

--- lib/signs_ui_web/controllers/auth_controller.ex
+++ lib/signs_ui_web/controllers/auth_controller.ex
@@ -3,7 +3,9 @@ defmodule SignsUiWeb.AuthController do
   plug(Ueberauth)
   require Logger
 
-  @spec callback(Plug.Conn.t(), map()) :: Plug.Conn.t()
+  alias Plug.Conn
+
+  @spec callback(Conn.t(), map()) :: Conn.t()
   def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
     username = auth.uid
     expiration = auth.credentials.expires_at
@@ -11,19 +13,28 @@ defmodule SignsUiWeb.AuthController do
 
     current_time = System.system_time(:second)
 
+    keycloak_client_id =
+      get_in(Application.get_env(:ueberauth_oidcc, :providers), [:keycloak, :client_id])
+
+    roles =
+      get_in(auth.extra.raw_info.userinfo, ["resource_access", keycloak_client_id, "roles"]) || []
+
     if credentials.refresh_token do
       refresh_token_store = Application.get_env(:signs_ui, :refresh_token_store)
       refresh_token_store.put_refresh_token(username, credentials.refresh_token)
     end
 
+    {:ok, logout_uri} = logout_uri(auth)
+
     conn
+    |> put_session("logout_uri", logout_uri)
     |> Guardian.Plug.sign_in(
       SignsUiWeb.AuthManager,
       username,
-      %{groups: credentials.other[:groups]},
+      %{roles: roles},
       ttl: {expiration - current_time, :seconds}
     )
-    |> Plug.Conn.put_session(:signs_ui_username, username)
+    |> Conn.put_session(:signs_ui_username, username)
     |> redirect(to: SignsUiWeb.Router.Helpers.messages_path(conn, :index))
   end
 
@@ -35,12 +46,7 @@ defmodule SignsUiWeb.AuthController do
 
     cond do
       error?(errors, "refresh_token_failure") ->
-        refresh_token_store = Application.get_env(:signs_ui, :refresh_token_store)
-
-        conn
-        |> Plug.Conn.fetch_session()
-        |> Plug.Conn.get_session(:signs_ui_username)
-        |> refresh_token_store.clear_refresh_token()
+        refresh_token_cleanup(conn)
 
         reauthenticate(conn)
 
@@ -52,20 +58,30 @@ defmodule SignsUiWeb.AuthController do
     end
   end
 
-  @spec logout(Plug.Conn.t(), map()) :: Plug.Conn.t()
-  def logout(conn, params) do
-    refresh_token_store = Application.get_env(:signs_ui, :refresh_token_store)
+  @spec logout(Conn.t(), map()) :: Conn.t()
+  def logout(conn, _params) do
+    refresh_token_cleanup(conn)
 
-    conn
-    |> Plug.Conn.fetch_session()
-    |> Plug.Conn.get_session(:signs_ui_username)
-    |> refresh_token_store.clear_refresh_token()
+    logout_uri = Conn.get_session(conn, "logout_uri")
 
-    redirect_url = logout_redirect_url_for_provider(conn, params["provider"])
+    redirect_to =
+      if is_nil(logout_uri),
+        do: [to: "/"],
+        else: [external: logout_uri]
 
     conn
     |> Guardian.Plug.sign_out(SignsUiWeb.AuthManager)
-    |> redirect(external: redirect_url)
+    |> Conn.clear_session()
+    |> redirect(redirect_to)
+  end
+
+  defp refresh_token_cleanup(conn) do
+    refresh_token_store = Application.get_env(:signs_ui, :refresh_token_store)
+
+    conn
+    |> Conn.fetch_session()
+    |> Conn.get_session(:signs_ui_username)
+    |> refresh_token_store.clear_refresh_token()
   end
 
   @spec error?([Ueberauth.Failure.t(), ...], String.t()) :: boolean
@@ -73,36 +89,25 @@ defmodule SignsUiWeb.AuthController do
     Enum.any?(errors, fn e -> e.message_key == key end)
   end
 
-  @spec reauthenticate(Plug.Conn.t()) :: Plug.Conn.t()
+  @spec reauthenticate(Conn.t()) :: Conn.t()
   defp reauthenticate(conn) do
     Phoenix.Controller.redirect(
       conn,
-      to: SignsUiWeb.Router.Helpers.auth_path(conn, :request, "cognito")
+      to: SignsUiWeb.Router.Helpers.auth_path(conn, :request, "keycloak")
     )
   end
 
-  @spec logout_redirect_url_for_provider(Plug.Conn.t(), String.t()) :: String.t()
-  defp logout_redirect_url_for_provider(conn, "cognito") do
-    auth_domain =
-      :ueberauth
-      |> Application.get_env(Ueberauth.Strategy.Cognito)
-      |> Keyword.get(:auth_domain)
-      |> config_value
-
-    redirect_params =
-      URI.encode_query(%{
-        "client_id" =>
-          :ueberauth
-          |> Application.get_env(Ueberauth.Strategy.Cognito)
-          |> Keyword.get(:client_id)
-          |> config_value,
-        "logout_uri" => SignsUiWeb.Router.Helpers.page_url(conn, :index)
-      })
-
-    "https://#{auth_domain}/logout?" <> redirect_params
+  @spec logout_uri(map()) :: {:ok, String.t()}
+  defp logout_uri(%Ueberauth.Auth{strategy: SignsUi.Ueberauth.Strategy.Fake}) do
+    {:ok, "/"}
   end
 
-  @spec config_value(binary() | {module(), atom(), [any()]}) :: any()
-  defp config_value(value) when is_binary(value), do: value
-  defp config_value({m, f, a}), do: apply(m, f, a)
+  defp logout_uri(%Ueberauth.Auth{strategy: Ueberauth.Strategy.Oidcc} = auth) do
+    logout_params = %{post_logout_redirect_uri: "/"}
+    UeberauthOidcc.initiate_logout_url(auth, logout_params)
+  end
+
+  # @spec config_value(binary() | {module(), atom(), [any()]}) :: any()
+  # defp config_value(value) when is_binary(value), do: value
+  # defp config_value({m, f, a}), do: apply(m, f, a)
 end
