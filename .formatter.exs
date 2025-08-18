482b3d726907322b7da87a60cfec2c907897b90a
Cora Grant <cgrant2@mbta.com>
2025-08-18 14:21:46 -0400

    chore: update Elixir formatter config
    * Add an `import_deps` option for Phoenix. This allows formatting macros
      provided by the framework without parens, e.g. `assert_broadcast`.
    
    * The `rename_deprecated_at` option no longer exists. See:
      https://github.com/elixir-lang/elixir/commit/b4f6f2a30c8d40170953ab69262578f01299a91e

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,4 @@
 [
-  inputs: ["mix.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  rename_deprecated_at: "1.6.0"
+  import_deps: [:phoenix],
+  inputs: ["mix.exs", "{config,lib,test}/**/*.{ex,exs}"]
 ]
